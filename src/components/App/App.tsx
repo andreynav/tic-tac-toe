7bcaeb8e492c45f22f2405dc97930cbf05ab9a14
andreinalivaika <nav.testsw@gmail.com>
2023-02-16 18:04:31 +0300

    5 add cross and nil marks

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,96 @@
 import { Board } from 'components/Board/Board'
+import { BoardRow } from 'components/BoardRow/BoardRow'
+import { Info } from 'components/Info/Info'
 import { Square } from 'components/Square/Square'
+import { useState } from 'react'
 import styled from 'styled-components'
 
-import { BoardRow } from '../BoardRow/BoardRow'
+export const getMark = (mark: boolean) => {
+  return mark ? 'X' : 'O'
+}
 
 export const App = () => {
+  const [isCross, setIsCross] = useState(true)
+  const [steps, setSteps] = useState([{ id: 1, text: 'Go to game start' }])
+
   return (
     <AppWrapper>
       <Board>
         <BoardRow>
-          <Square />
-          <Square />
-          <Square />
+          <Square
+            cellId={1}
+            mark={isCross}
+            setMark={setIsCross}
+            steps={steps}
+            setSteps={setSteps}
+          />
+          <Square
+            cellId={2}
+            mark={isCross}
+            setMark={setIsCross}
+            steps={steps}
+            setSteps={setSteps}
+          />
+          <Square
+            cellId={3}
+            mark={isCross}
+            setMark={setIsCross}
+            steps={steps}
+            setSteps={setSteps}
+          />
         </BoardRow>
         <BoardRow>
-          <Square />
-          <Square />
-          <Square />
+          <Square
+            cellId={4}
+            mark={isCross}
+            setMark={setIsCross}
+            steps={steps}
+            setSteps={setSteps}
+          />
+          <Square
+            cellId={5}
+            mark={isCross}
+            setMark={setIsCross}
+            steps={steps}
+            setSteps={setSteps}
+          />
+          <Square
+            cellId={6}
+            mark={isCross}
+            setMark={setIsCross}
+            steps={steps}
+            setSteps={setSteps}
+          />
         </BoardRow>
         <BoardRow>
-          <Square />
-          <Square />
-          <Square />
+          <Square
+            cellId={7}
+            mark={isCross}
+            setMark={setIsCross}
+            steps={steps}
+            setSteps={setSteps}
+          />
+          <Square
+            cellId={8}
+            mark={isCross}
+            setMark={setIsCross}
+            steps={steps}
+            setSteps={setSteps}
+          />
+          <Square
+            cellId={9}
+            mark={isCross}
+            setMark={setIsCross}
+            steps={steps}
+            setSteps={setSteps}
+          />
         </BoardRow>
       </Board>
+      <Info>
+        {steps.map((step) => (
+          <div key={step.id}>{`${step.id}. ${step.text}`}</div>
+        ))}
+      </Info>
     </AppWrapper>
   )
 }
@@ -33,10 +100,10 @@ const AppWrapper = styled.div`
   height: 100vh;
   width: auto;
   grid-template-rows: 0.7fr auto 0.7fr;
-  grid-template-columns: 0.7fr auto 0.7fr;
+  grid-template-columns: 0.5fr auto 0.8fr;
   background-color: #282c34;
   grid-template-areas:
     '. . .'
-    '. Board .'
+    '. Board Info'
     '. . .';
 `
--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,19 +1,31 @@
 import { useState } from 'react'
 import styled from 'styled-components'
 
-// type PropsT = {
-//   value: string
-//   // onClick?: () => void
-// }
+import { getMark } from '../App/App'
 
-export const Square = () => {
+type StepsT = {
+  id: number
+  text: string
+}
+
+type PropsT = {
+  cellId: number
+  mark: boolean
+  setMark: (mark: boolean) => void
+  steps: Array<StepsT>
+  setSteps: (arr: Array<StepsT>) => void
+}
+
+export const Square = ({ cellId, mark, setMark, steps, setSteps }: PropsT) => {
   const [value, setValue] = useState('')
+  const nextId = steps[steps.length - 1]!.id + 1
+
   const onSquareClick = () => {
-    if (!value) {
-      setValue('X')
-    } else {
-      setValue('')
-    }
+    if (value) return
+
+    setValue(getMark(mark))
+    setMark(!mark)
+    setSteps([...steps, { id: nextId, text: `Step was made in cell #${cellId}` }])
   }
 
   return <StyledButton onClick={onSquareClick}>{value}</StyledButton>
